970774b359b6aa1096e61312efb19ae3045a8ae3
kt0mmy <tommydsha@gmail.com>
2026-08-18 23:31:11 +0900

    フォントデータをオブジェクトファイルとしてリンクする

--- kernel/font.cpp
+++ kernel/font.cpp
@@ -1,34 +1,28 @@
 #include "font.hpp"
 
-const uint8_t kFontA[16] = {
-    0b00000000, //
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00011000, //    **
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b00100100, //   *  *
-    0b01111110, //  ******
-    0b01000010, //  *    *
-    0b01000010, //  *    *
-    0b01000010, //  *    *
-    0b11100111, // ***  ***
-    0b00000000, //
-    0b00000000, //
-};
+// objcopy で生成したオブジェクトファイルで定義されているシンボル
+extern const uint8_t _binary_hankaku_bin_start; // データの開始仮想アドレス(データを格納する変数ではなく、データの先頭位置を示すリンカシンボル)
+extern const uint8_t _binary_hankaku_bin_end; // 終了アドレス + 1
+extern const uint8_t _binary_hankaku_bin_size; // データのトータルサイズ(バイト)
+
+const uint8_t* GetFont(char c) {
+    auto index = 16 * static_cast<unsigned int>(c); // 1 文字は16バイト(1行は8ビット)
+    if (index >= reinterpret_cast<uintptr_t>(&_binary_hankaku_bin_size)) {
+        return nullptr;
+    }
+    return &_binary_hankaku_bin_start + index;
+}
 
 void WriteAscii(PixelWriter &writer, int x, int y, char c, const PixelColor &color)
 {
-    if (c != 'A')
-        return;
+    const uint8_t* font = GetFont(c);
+    if (font == nullptr) return;
 
     for (int dy = 0; dy < 16; dy++)
     {
         for (int dx = 0; dx < 8; dx++)
         {
-            if (kFontA[dy] & (1 << dx))
+            if ((font[dy] << dx) & 0x80u)
             {
                 writer.Write(x + dx, y + dy, color);
             }
--- kernel/main.cpp
+++ kernel/main.cpp
@@ -65,8 +65,10 @@ extern "C" void KernelMain(const FrameBuferConfig &frame_buffer_config)
         }
     }
 
-    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+    int i = 0;
+    for (char c='!';c<='~';c++, i++) {
+        WriteAscii(*pixel_writer, 50 + i * 8, 50, c, {0, 0, 0});
+    }
     while (1)
         __asm__("hlt");
 }
