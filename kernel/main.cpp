d6d38f5c47f268d16da17a6e4af1a487f00dd5d6
kt0mmy <tommydsha@gmail.com>
2026-08-19 22:27:25 +0900

    マウスカーソルを描画

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -47,9 +47,10 @@ char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
 
 char console_buf[sizeof(Console)];
-Console* console;
+Console *console;
 
-int printk(const char* format, ...) {
+int printk(const char *format, ...)
+{
     va_list ap;
     int result;
     char s[1024];
@@ -61,6 +62,38 @@ int printk(const char* format, ...) {
     console->PutString(s);
     return result;
 }
+
+const PixelColor kDesktopBGColor{45, 118, 237};
+const PixelColor kDesktopFGColor{255, 255, 255};
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+
+const char mouse_cursor_shape[kMouseCursorHeight][kMouseCursorWidth + 1] = {
+    "@              ",
+    "@@             ",
+    "@.@            ",
+    "@..@           ",
+    "@...@          ",
+    "@....@         ",
+    "@.....@        ",
+    "@......@       ",
+    "@.......@      ",
+    "@........@     ",
+    "@.........@    ",
+    "@..........@   ",
+    "@...........@  ",
+    "@............@ ",
+    "@......@@@@@@@@",
+    "@......@       ",
+    "@....@@.@      ",
+    "@...@ @.@      ",
+    "@..@   @.@     ",
+    "@.@    @.@     ",
+    "@@      @.@    ",
+    "@       @.@    ",
+    "         @.@   ",
+    "         @@@   "};
 
 // NOTE: マングリングを防ぐ
 extern "C" void KernelMain(const FrameBuferConfig &frame_buffer_config)
@@ -75,13 +108,45 @@ extern "C" void KernelMain(const FrameBuferConfig &frame_buffer_config)
         break;
     }
 
-    console = new(console_buf) Console{*pixel_writer, {0, 0, 0}, {255, 255, 255}};
+    const int kFrameWidth = frame_buffer_config.horizontal_resolution;
+    const int kFrameHeight = frame_buffer_config.vertical_resolution;
 
-    for (int row = 0; row < 27; row++)
+    // #@@range_begin(draw_desktop)
+    FillRectangle(*pixel_writer,
+                  {0, 0},
+                  {kFrameWidth, kFrameHeight - 50},
+                  kDesktopBGColor);
+    FillRectangle(*pixel_writer,
+                  {0, kFrameHeight - 50},
+                  {kFrameWidth, 50},
+                  {1, 8, 17});
+    FillRectangle(*pixel_writer,
+                  {0, kFrameHeight - 50},
+                  {kFrameWidth / 5, 50},
+                  {80, 80, 80});
+    DrawRectangle(*pixel_writer,
+                  {10, kFrameHeight - 40},
+                  {30, 30},
+                  {160, 160, 160});
+
+    console = new (console_buf) Console{*pixel_writer, kDesktopFGColor, kDesktopBGColor};
+
+    for (int dy = 0; dy < kMouseCursorHeight; dy++)
     {
-        printk("printk: %d\n", row);
+        for (int dx = 0; dx < kMouseCursorWidth; dx++)
+        {
+            if (mouse_cursor_shape[dy][dx] == '@')
+            {
+                pixel_writer->Write(200 + dx, 100 + dy, {0, 0, 0});
+            }
+            else if (mouse_cursor_shape[dy][dx] == '.')
+            {
+                pixel_writer->Write(200 + dx, 100 + dy, {255, 255, 255});
+            }
+        }
     }
 
+    printk("Hello, MyOS!");
     while (1)
         __asm__("hlt");
 }
